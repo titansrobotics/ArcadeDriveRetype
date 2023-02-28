e3a3edf9d0c739c56fc9bdd09a6489a7f2e56f5a
titansrobotics <titanrobotics8174@gmail.com>
2023-02-28 16:41:51 -0700

    just a push

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -5,6 +5,9 @@ import edu.wpi.first.wpilibj.Encoder;
 public class Autonomous {
     private static Encoder driveEncoder = new Encoder(0,1);
 
+    private static whd = 6;
+
+
     driveEncoder.getDistancePerPulse();
 
 }
